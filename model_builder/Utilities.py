dcf52a9de83610019bd08622db2148651c5b8517
heroico <soyunheroe@gmail.com>
2016-02-09 09:44:32 -0300

    File Parsing might be wrapped in try/catch. Or not.

--- model_builder/Utilities.py
+++ model_builder/Utilities.py
@@ -1,12 +1,18 @@
 import gzip
 import logging
 
-def parse_file(path, callback):
-    with gzip.open(path) as file:
-        try:
+def parse_file(path, callback, trow=False):
+    def parse(path, callback):
+        with gzip.open(path) as file:
             for i, line in enumerate(file):
                 callback(i, line.strip().split())
+
+    if trow:
+        try:
+            parse(path, callback)
         except IOError:
             logging.info("Expected bad file, and that's what we got: %s" % (path, ))
         except Exception as e:
             raise e
+    else:
+        parse(path, callback)
